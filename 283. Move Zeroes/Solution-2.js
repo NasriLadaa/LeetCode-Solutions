d12962ebabe7c2a695f3213d35bbea604718481f
Nasri_ladaa <nasrelada@gmail.com>
2025-03-10 21:03:55 +0200

    283. Moves Zeroes
    283. Moves Zeroes

---
+++ 283. Move Zeroes/Solution-2.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+
+    let left = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            nums[left] = nums[i]
+            left++
+        }
+    }
+
+    while (left < nums.length) {
+        nums[left] = 0;
+        left++
+    }
+
+    return nums;
+};
