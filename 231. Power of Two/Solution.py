ddfa7e5f9bcdd82c5367c501866b1371eac47466
Nasri_ladaa <nasrelada@gmail.com>
2025-06-29 21:50:04 +0300

    Power of Two
    Power of Two

---
+++ 231. Power of Two/Solution.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/power-of-two/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if ( n <- 0):
+            return False
+        x = 1
+        while ( x < n):
+            x *= 2
+        return x == n
+        
